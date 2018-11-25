3680580cd5681d0500660c6175a96f4c7212156b
CIOC <you@example.com>
2018-11-25 19:07:54 +0530

    new begning

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Classes from './App.module.css';
+import Header from './Components/Header'
 
 class App extends Component {
+  state={
+    imgs:[{
+      src='./public/imgs/image1.jpg'
+    },
+    {
+      src='./public/imgs/image3.jpg'
+    },
+    {
+      src='./public/imgs/image2.jpg'
+    }]
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <Header images={this.state.imgsHeader}/>
       </div>
     );
   }
---
+++ src/Components/Balram.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Classes from './Balram.module.scss';
+
+class Balram extends Component {
+  render() {
+    return (
+      <div>
+        <header className={Classes.Balram}>
+          Here
+        </header>
+      </div>
+    );
+  }
+}
+
+export default Balram;
