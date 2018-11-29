d431caf90afc282c443fb97af88111d4f7a67d6f
CIOC <you@example.com>
2018-11-29 16:36:22 +0530

    avd

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,18 +3,31 @@ import Class from './Navbar.module.scss';
 import ShopList from '../ShopList/ShopList';
 
 class Navbar extends Component {
+  state={
+    showShopList:false
+  }
+  showBar=()=>{
+    var current=this.state.showShopList
+    this.setState({
+      showShopList:!current
+    })
+  }
 
   render() {
+    var content=null;
+    if(this.state.showShopList){
+      content= <ShopList/>
+    }
     return (
       <div className={Class.navbar}>
         <ul className={Class.list}>
-          <li>Shop</li>
+          <li onClick={this.showBar}>Shop</li>
           <li>About us</li>
           <li>Blog</li>
           <li>Offers</li>
           <li>Search</li>
         </ul>
-        <ShopList/>
+        {content}
       </div>
     );
   }
